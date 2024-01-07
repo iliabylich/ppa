5298287ebe3277e8675ae62346464ad3577a309c
Ilya Bylich <ibylich@gmail.com>
2024-01-08 00:09:03 +0100

    update hnparser

--- refresh.sh
+++ refresh.sh
@@ -2,5 +2,5 @@ dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
 
 apt-ftparchive release . > Release
-gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg
-gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
+gpg --default-key "ibylich@gmail.com" -abs -o - Release > Release.gpg
+gpg --default-key "ibylich@gmail.com" --clearsign -o - Release > InRelease

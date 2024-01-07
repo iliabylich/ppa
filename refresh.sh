6803cf8f0dbed856a50109e117c154a8344e47eb
Ilya Bylich <ibylich@gmail.com>
2024-01-07 21:05:22 +0100

    add first package

---
+++ refresh.sh
@@ -0,0 +1,6 @@
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg
+gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease

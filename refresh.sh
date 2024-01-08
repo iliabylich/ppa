5697c918833cb3ba6497699ab0765071206b8e5b
Ilya Bylich <ibylich@gmail.com>
2024-01-08 01:10:44 +0100

    re-generate index on action dispatch

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+
+set -eux
+
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
 
 apt-ftparchive release . > Release
+
 gpg --default-key "ibylich@gmail.com" -abs -o - Release > Release.gpg
 gpg --default-key "ibylich@gmail.com" --clearsign -o - Release > InRelease
